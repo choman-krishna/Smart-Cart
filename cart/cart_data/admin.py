13dbba7dcc4f9f19363bf53d783485e88dd3dbbe
Chummi <95726160+choman-krishna@users.noreply.github.com>
2024-05-05 18:51:31 +0530

    scanned item model created

--- cart/cart_data/admin.py
+++ cart/cart_data/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
-from cart_data.models import ItemData
+from cart_data.models import ItemData, ScannedItem
 
 class ItemDataAdmin(admin.ModelAdmin):
     list_display = ('item_name', 'bar_data', 'prize', 'item_img')
 
+class ScannedItemAdmin(admin.ModelAdmin):
+    list_display = ("user_name", "scanned_item", "in_cart")
 
 # Register your models here.
 admin.site.register(ItemData, ItemDataAdmin)
+admin.site.register(ScannedItem, ScannedItemAdmin)
--- cart/cart_data/models.py
+++ cart/cart_data/models.py
@@ -6,4 +6,10 @@ class ItemData(models.Model):
     prize = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     item_img = models.ImageField(upload_to = 'item_img/', max_length = 300, null=True, default = None)
 
+class ScannedItem(models.Model):
+    # remove default afterwards
+    user_name = models.TextField(max_length=10, default="me")
+    scanned_item = models.TextField(max_length=20)
+    in_cart = models.BooleanField(default=False)
+
 # Create your models here.
